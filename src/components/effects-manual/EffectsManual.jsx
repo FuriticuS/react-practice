c25e5b0f5ca6a9d67e412e72e69bfc21a8ec3d8a
Fur1ouS <Fur1ouS@ya.ru>
2026-03-03 02:21:31 +0300

    add async requests

--- src/components/effects-manual/EffectsManual.jsx
+++ src/components/effects-manual/EffectsManual.jsx
@@ -1,11 +1,26 @@
 import Button from "../button/Button.jsx";
 import Modal from "../modal/Modal.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ModalRoot from "../modal/ModalRoot.jsx";
 
 export default function EffectsManual() {
   const [modal, setModal] = useState(false)
   const [isModal, setIsModal] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function fetchUsers() {
+      setLoading(true);
+      const resp = await fetch('https://jsonplaceholder.typicode.com/users/')
+      const users = await resp.json()
+
+      setUsers(users)
+      setLoading(false);
+    }
+
+    fetchUsers();
+  }, [])
 
   function openModal() {
     setModal(!modal)
@@ -40,6 +55,12 @@ export default function EffectsManual() {
 
         <Button functionClick={() => setIsModal(false)} title={"Close"}/>
       </ModalRoot>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>}
     </div>
   )
 }
